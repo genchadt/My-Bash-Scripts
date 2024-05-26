748d9fbd6e418a244e74ad8f8e7103e73e5a8341
genchadt <62498018+genchadt@users.noreply.github.com>
2024-05-26 16:57:23 -0400

    aaaaaaaaaaaaaah

--- check-updates.sh
+++ check-updates.sh
@@ -61,12 +61,6 @@ SERVER_TIME=$(date)
 # Collect uptime
 UPTIME_INFO=$(uptime -p)
 
-if ! command -v csvtool &> /dev/null
-then
-    echo "csvtool could not be found, please install it."
-    exit 1
-fi
-
 # Extract and format CrowdSec alerts
 CSCLI_ALERTS=$(cscli alerts list -o raw | csvtool format '<tr><td>%1</td><td>%2</td><td>%3</td><td>%4</td><td>%5</td><td>%6</td><td>%7</td><td>%8</td></tr>\n' - | {
     echo "<table border=\"1\"><tr><th>ID</th><th>Scope</th><th>Value</th><th>Reason</th><th>Country</th><th>AS</th><th>Decisions</th><th>Created At</th></tr>"
