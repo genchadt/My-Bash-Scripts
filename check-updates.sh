adba590e1798afcecb24244710dc7915ca443e80
genchadt <62498018+genchadt@users.noreply.github.com>
2024-06-01 08:30:36 -0400

    fix ?

--- check-updates.sh
+++ check-updates.sh
@@ -6,8 +6,7 @@ UPTIME_INFO=$(uptime -p)
 
 # Collect and format package and distribution updates
 apt-get update -y
-UPGRADE_LIST=$(apt list --upgradable 2>/dev/null | grep -E 'upgradable from' | awk -F'[][]' '{print $2 " " $3}')
-DIST_UPGRADE_LIST=$(apt-get -s dist-upgrade | grep "^Inst" | awk '{print $2}')
+UPGRADE_LIST=$(apt list --upgradable 2>/dev/null | grep -E 'upgradable from' | awk -F'[][]' '{print $1 " " $2}' | awk -F' ' '{print $1 " " $4 " " $6}')
 
 if [ -z "$UPGRADE_LIST" ]; then
     FORMATTED_UPGRADE_LIST="<p>All packages are up to date.</p>"
@@ -15,26 +14,13 @@ else
     FORMATTED_UPGRADE_LIST="<table border='1'><tr><th>Package</th><th>Current Version</th><th>New Version</th></tr>"
     while read -r line; do
         PACKAGE=$(echo "$line" | awk '{print $1}')
-        VERSIONS=$(echo "$line" | awk '{print $2}')
-        CURRENT_VERSION=$(echo "$VERSIONS" | awk -F'/' '{print $1}')
-        NEW_VERSION=$(echo "$VERSIONS" | awk -F'/' '{print $2}')
+        CURRENT_VERSION=$(echo "$line" | awk '{print $2}')
+        NEW_VERSION=$(echo "$line" | awk '{print $3}')
         FORMATTED_UPGRADE_LIST+="<tr><td>${PACKAGE}</td><td>${CURRENT_VERSION}</td><td>${NEW_VERSION}</td></tr>"
     done <<EOF
 $UPGRADE_LIST
 EOF
     FORMATTED_UPGRADE_LIST+="</table>"
-fi
-
-if [ -z "$DIST_UPGRADE_LIST" ]; then
-    FORMATTED_DIST_UPGRADE_LIST="<p>Distribution is up to date.</p>"
-else
-    FORMATTED_DIST_UPGRADE_LIST="<table border='1'><tr><th>Package</th></tr>"
-    while read -r line; do
-        FORMATTED_DIST_UPGRADE_LIST+="<tr><td>$line</td></tr>"
-    done <<EOF
-$DIST_UPGRADE_LIST
-EOF
-    FORMATTED_DIST_UPGRADE_LIST+="</table>"
 fi
 
 # Collect and format disk information
@@ -183,8 +169,6 @@ function toggleSpoiler(id) {
 <p>$UPTIME_INFO</p>
 <h2>Available Package Updates:</h2>
 <p>$FORMATTED_UPGRADE_LIST</p>
-<h2>Available Distribution Upgrades:</h2>
-<p>$FORMATTED_DIST_UPGRADE_LIST</p>
 <h2>Disk Information:</h2>
 <p>$FORMATTED_DISK_INFO</p>
 <h2>Memory Usage:</h2>
