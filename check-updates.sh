784ed4bd17d1bab6dd1a742425743a32461a28cf
genchadt <62498018+genchadt@users.noreply.github.com>
2024-06-02 14:23:01 -0400

    test2

--- check-updates.sh
+++ check-updates.sh
@@ -33,24 +33,24 @@ BEGIN {
 }
 NR==1 {
     print "
-    <tr>
-        <th>" $1 "</th>
-        <th>" $2 "</th>
-        <th>" $3 "</th>
-        <th>" $4 "</th>
-        <th>" $5 "</th>
-        <th>Mounted on</th>
+    <tr>\
+        <th>" $1 "</th>\
+        <th>" $2 "</th>\
+        <th>" $3 "</th>\
+        <th>" $4 "</th>\
+        <th>" $5 "</th>\
+        <th>Mounted on</th>\
     </tr>"
 }
 NR>1 {
     print "
-    <tr>
-        <td>" $1 "</td>
-        <td>" $2 "</td>
-        <td>" $3 "</td>
-        <td>" $4 "</td>
-        <td>" $5 "</td>
-        <td>" $6 "</td>
+    <tr>\
+        <td>" $1 "</td>\
+        <td>" $2 "</td>\
+        <td>" $3 "</td>\
+        <td>" $4 "</td>\
+        <td>" $5 "</td>\
+        <td>" $6 "</td>\
     </tr>"
 }
 END {
