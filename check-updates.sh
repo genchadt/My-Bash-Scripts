9a8abe93fefca9240aefeff93ccc7feb47d24b74
genchadt <62498018+genchadt@users.noreply.github.com>
2024-08-18 00:24:56 -0400

    fix

--- check-updates.sh
+++ check-updates.sh
@@ -150,21 +150,18 @@ END {
 PREVIOUS_SSH_SESSIONS=$(last -n 10 | awk '
 BEGIN {
     login_count = 0
-    print "<table border=\"1\"><tr><th>User</th><th>Type</th><th>Terminal</th><th>IP Address</th><th>Login Time</th><th>Duration</th></tr>"
+    print "<table border=\"1\"><tr><th>User</th><th>Terminal</th><th>IP Address</th><th>Login Time</th><th>Duration</th></tr>"
 }
-/^$/ { next }  # Skip empty lines
-/^wtmp/ { next }  # Skip "wtmp begins" lines
+/^$/ { next }
+/^wtmp/ { next }
+/^reboot/ { next }
 {
-    if ($1 == "reboot") {
-        print "<tr><td>" $1 "</td><td>" $2 "</td><td>" $3 "</td><td></td><td>" $4 "</td><td>" $5 "</td></tr>"
-    } else {
-        print "<tr><td>" $1 "</td><td>user</td><td>" $2 "</td><td>" $3 "</td><td>" $4 " " $5 "</td><td>" $6 "</td></tr>"
-    }
+    print "<tr><td>" $1 "</td><td>" $2 "</td><td>" $3 "</td><td>" $4 " " $5 "</td><td>" $6 "</td></tr>"
     login_count++
 }
 END {
     if (login_count == 0) {
-        print "<p>No recent SSH logins or reboots found.</p>"
+        print "<p>No recent SSH logins found.</p>"
     } else {
         print "</table>"
     }
